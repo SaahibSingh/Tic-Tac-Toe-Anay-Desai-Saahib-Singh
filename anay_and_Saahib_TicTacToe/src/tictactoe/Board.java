36188dcd6d73db1ca1a92d6b2e9c4abfebf5ba8a
Anay-Desai <anaydesai2011@gmail.com>
2026-03-05 13:43:05 -0500

    changed the format otherwise i wont listen

--- anay_and_Saahib_TicTacToe/src/tictactoe/Board.java
+++ anay_and_Saahib_TicTacToe/src/tictactoe/Board.java
@@ -5,19 +5,22 @@ import java.io.File;
 import java.io.FileWriter;
 import java.util.Scanner;
 
-public class Board {
+public class Board 
+{
     private char[][] grid; //holds game play data in an instance variable
     private String filename; //holds game play data in a CSV file
         
     //non-default constructor - [5 points]
-    public Board(String filename) {
+    public Board(String filename) 
+    {
     	 //set the file name
        //if the board is valid then create the 3x3 grid
        //and load the board from the file
     }
     
     //loads the grid with the file contents - [5 points]
-    public void loadBoardFromFile() {
+    public void loadBoardFromFile() 
+    {
         //Use a scanner to read the board file
         //and populate the grid with the board values
         //remember to close the scanner afterwards 
@@ -25,17 +28,35 @@ public class Board {
     }
  
     //valid if it resembles a 3x3 board that contains only E, X, O
-    public boolean isValidBoardFile() { }
+    public boolean isValidBoardFile() 
+    {
+    	
+    }
     
     //saves the grid to the file in the proper format (CSV)
-    public void saveBoardToFile() { }
+    public void saveBoardToFile() 
+    { 
+    	
+    }
     
     /***These are the methods used to test those above***/
-    public void printGrid() { } //prints the current grid
-    public void createRandomBoard() { } //create a random board
-    public void clearBoard()  {  } //clears the grid by placing E in every cell
+    public void printGrid() 
+    { 
+    	
+    } //prints the current grid
     
-    public static void main(String args[]) {
+    public void createRandomBoard() 
+    { 
+    	
+    } //create a random board
+    
+    public void clearBoard()  
+    {  
+    	
+    } //clears the grid by placing E in every cell
+    
+    public static void main(String args[]) 
+    {
     	Board b = new Board("board.csv");
     	System.out.println(b.isValidBoardFile());
     	b.createRandomBoard();
