609ccd61dc603c5f914721c10ba2709cb5e81c6a
Saahib M. Singh <saahibmaansingh@gmail.com>
2026-05-02 16:04:37 -0400

    Refactor User class constructor and fields

--- anay_and_Saahib_TicTacToe/src/tictactoe/User.java
+++ anay_and_Saahib_TicTacToe/src/tictactoe/User.java
@@ -1,14 +1,12 @@
 package tictactoe;
 public class User {
-    private final String username;
-    private final String passwordHash;
-    private final String avatar;
-    public User(String username, String passwordHash) {
+    private final String username, passwordHash, String avatar;
+    public User(String username, String passwordHash, String avatar) {
         this.username = username;
         this.passwordHash = passwordHash;
+        this.avatar = avatar;
     }
     public String getUsername() { return username; }
     public String getPasswordHash() { return passwordHash; }
-    
     public String getAvatar() { return avatar; }
 }
