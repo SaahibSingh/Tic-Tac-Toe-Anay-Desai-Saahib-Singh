7f6641ed65ef8b625b054e2e72f76362025b6412
Saahib M. Singh <saahibmaansingh@gmail.com>
2026-05-02 14:31:50 -0400

    Simplify getUser and destroy methods
    Refactor SessionManager methods for brevity.

--- anay_and_Saahib_TicTacToe/src/tictactoe/SessionManager.java
+++ anay_and_Saahib_TicTacToe/src/tictactoe/SessionManager.java
@@ -1,22 +1,13 @@
 package tictactoe;
-
-import java.util.*;
-
+import java.util.*; //Import
 public class SessionManager {
-
-    private final Map<String, String> tokenToUser = new HashMap<>();
-
+    private final Map<String, String> tokenToUser = new HashMap<>(); //Instance Variables
     public String createSession(String username) {
         String token = UUID.randomUUID().toString();
         tokenToUser.put(token, username);
         return token;
     }
 
-    public String getUser(String token) {
-        return tokenToUser.get(token);
-    }
-
-    public void destroy(String token) {
-        tokenToUser.remove(token);
-    }
+    public String getUser(String token) { return tokenToUser.get(token); }
+    public void destroy(String token) { tokenToUser.remove(token); }
 }
