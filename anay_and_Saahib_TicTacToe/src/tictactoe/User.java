df1901e73b84424799f7f0a117ededda26c56cd3
Saahib M. Singh <saahibmaansingh@gmail.com>
2026-05-02 16:03:40 -0400

    Add avatar field and getter to User class
    Added avatar field and corresponding getter method.

--- anay_and_Saahib_TicTacToe/src/tictactoe/User.java
+++ anay_and_Saahib_TicTacToe/src/tictactoe/User.java
@@ -2,10 +2,13 @@ package tictactoe;
 public class User {
     private final String username;
     private final String passwordHash;
+    private final String avatar;
     public User(String username, String passwordHash) {
         this.username = username;
         this.passwordHash = passwordHash;
     }
     public String getUsername() { return username; }
     public String getPasswordHash() { return passwordHash; }
+    
+    public String getAvatar() { return avatar; }
 }
