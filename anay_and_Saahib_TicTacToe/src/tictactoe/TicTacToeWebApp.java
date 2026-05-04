f543cece422e9afe46ed775803234a50b9318841
Anay-Desai <anaydesai2011@gmail.com>
2026-05-04 11:55:35 -0400

    ejgtrkerjfeskjf

--- anay_and_Saahib_TicTacToe/src/tictactoe/TicTacToeWebApp.java
+++ anay_and_Saahib_TicTacToe/src/tictactoe/TicTacToeWebApp.java
@@ -2,11 +2,9 @@ package tictactoe;
 
 public class TicTacToeWebApp {
     public static void main(String[] args) {
-        System.out.println("Backend running (no server).");
-        System.out.println("You can test game logic here.");
 
         // Example test:
-        Board board = new Board();
+        Board board = new Board("board.csv");
         GameLogic logic = new GameLogic();
 
         board.setCell(0, 0, 'X');
