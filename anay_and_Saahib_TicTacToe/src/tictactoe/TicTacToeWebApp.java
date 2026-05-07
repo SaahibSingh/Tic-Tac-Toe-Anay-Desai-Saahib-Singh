842eedd1c079c57bc6b828bc53fa03e4279167cf
Saahib M. Singh <saahibmaansingh@gmail.com>
2026-05-07 14:01:54 -0400

    Refactor TicTacToeWebApp for user input options

--- anay_and_Saahib_TicTacToe/src/tictactoe/TicTacToeWebApp.java
+++ anay_and_Saahib_TicTacToe/src/tictactoe/TicTacToeWebApp.java
@@ -1,86 +1,17 @@
 package tictactoe;
-
+import java.util.Scanner; //Import
 public class TicTacToeWebApp {
-
     public static void main(String[] args) {
-
-        char[][] board = {
-            {' ', ' ', ' '},
-            {' ', ' ', ' '},
-            {' ', ' ', ' '}
-        };
-
-        char current = 'X';
-
-        printBoard(board);
-
-        // Simulated moves (you can change these)
-        makeMove(board, 0, 0, current); current = switchPlayer(current);
-        makeMove(board, 1, 1, current); current = switchPlayer(current);
-        makeMove(board, 0, 1, current); current = switchPlayer(current);
-        makeMove(board, 2, 2, current); current = switchPlayer(current);
-        makeMove(board, 0, 2, current); // X wins here
-
-        printBoard(board);
-
-        if (checkWin(board, 'X')) {
-            System.out.println("X wins!");
-        } else if (checkWin(board, 'O')) {
-            System.out.println("O wins!");
-        } else if (isDraw(board)) {
-            System.out.println("It's a draw!");
-        } else {
-            System.out.println("Game still in progress.");
-        }
+        Scanner sc = new Scanner(System.in);
+        System.out.println("=== Tic Tac Toe ===");
+        System.out.println("1. Console Version");
+        System.out.println("2. Simple UI Version");
+        System.out.print("Choose an option: ");
+        int choice = sc.nextInt();
+        sc.nextLine(); 
+        if (choice == 1) ConsoleGame.start(sc);
+        else if (choice == 2) SimpleUI.start();
+        else System.out.println("Invalid choice.");
+        sc.close();
     }
-
-    static void printBoard(char[][] b) {
-        System.out.println("-------------");
-        for (int r = 0; r < 3; r++) {
-            System.out.print("| ");
-            for (int c = 0; c < 3; c++) {
-                System.out.print(b[r][c] + " | ");
-            }
-            System.out.println();
-            System.out.println("-------------");
-        }
-        System.out.println();
-    }
-
-    static void makeMove(char[][] b, int r, int c, char p) {
-        if (b[r][c] == ' ') {
-            b[r][c] = p;
-        }
-        printBoard(b);
-    }
-
-    static char switchPlayer(char p) {
-        return (p == 'X') ? 'O' : 'X';
-    }
-
-    static boolean checkWin(char[][] b, char p) {
-        // Rows
-        for (int r = 0; r < 3; r++)
-            if (b[r][0] == p && b[r][1] == p && b[r][2] == p)
-                return true;
-
-        // Columns
-        for (int c = 0; c < 3; c++)
-            if (b[0][c] == p && b[1][c] == p && b[2][c] == p)
-                return true;
-
-        // Diagonals
-        if (b[0][0] == p && b[1][1] == p && b[2][2] == p) return true;
-        if (b[0][2] == p && b[1][1] == p && b[2][0] == p) return true;
-
-        return false;
-    }
-
-    static boolean isDraw(char[][] b) {
-        for (int r = 0; r < 3; r++)
-            for (int c = 0; c < 3; c++)
-                if (b[r][c] == ' ')
-                    return false;
-        return true;
-    }
-    }
+}
